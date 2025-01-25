b83eb065a862e88a0e76177b07a433fc1ae43f15
Marco Marmolejos <marco.marmolejos@gmail.com>
2025-01-25 14:08:33 -0400

    Added more sounds to gameplay scene

---
+++ Assets/Game/Systems/Sound/RandomSound.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RandomSound : MonoBehaviour
+{
+    public AudioSource source;
+    public AudioClip[] audioList;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var clipId = Random.Range(0, audioList.Length);
+        source.clip = audioList[clipId];
+        source.Play();
+    }
+}
